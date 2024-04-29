95a43f14d48d938e82dc801d5091a15cefa436df
ShinoMeishu <82775097+ShinoMeishu@users.noreply.github.com>
2024-04-29 15:39:28 +0300

    Update and rename ISP IP and iptables config to isp_base_config.sh

--- isp_base_config.sh
+++ isp_base_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
 #ISP-Router config
+
 #Set ip addresses
 
 INT1="ens33"
@@ -17,9 +17,11 @@ echo -e "#Internet access interface\nauto $INT1\niface $INT1 inet dhcp\n" >> /et
 echo -e "#Subnet Internet \nauto $INT2 \niface $INT2 inet static \naddress $ADD1 \n" >> /etc/network/interfaces
 echo -e "#Subnet ISP-HQ\nauto $INT3\niface $INT3 inet static\naddress $ADD2 \n" >> /etc/network/interfaces
 echo -e "#Subnet ISP-BR\nauto $INT4\niface $INT4 inet static\naddress $ADD3"  >> /etc/network/interfaces
+
 echo "Added addresses"
 
-#Flush iptables
+#iptables install, flush and add
+apt install iptables -y
 sudo iptables -P INPUT ACCEPT
 sudo iptables -P FORWARD ACCEPT
 sudo iptables -P OUTPUT ACCEPT
@@ -27,6 +29,14 @@ sudo iptables -t nat -F
 sudo iptables -t mangle -F
 sudo iptables -F
 sudo iptables -A POSTROUTING -t nat -j MASQUERADE
+
+echo "iptables configured"
+
+#Ipv4 forwarding
+sudo sysctl -w net.ipv4.ip_forward=1
+sysctl -p
+
 systemctl restart networking
+echo "Addresses, ip tables and ipv4_forwarding configured"
 exit 0
 
