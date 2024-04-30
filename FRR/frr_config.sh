230f2c07c812529e2934a783b434c27841dc053c
ShinoMeishu <82775097+ShinoMeishu@users.noreply.github.com>
2024-04-30 16:26:58 +0300

    Create frr_config.sh

---
+++ FRR/frr_config.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+H=$(hostname)
+apt install frr -y
+#Configuration for Vtysh and ospfd daemon
+chmod 777 /etc/frr/daemons
+chown frr /etc/frr/daemons
+chmod 777 /etc/pam.d/frr
+sed -i 's/ospfd=no/ospfd=yes/' /etc/frr/daemons
+if [ "$H" == "ISP" ]; then
+echo -e "router ospf 1\n ospf router-id 10.1.1.1\n redistribute connected\n network 10.1.1.0/28 area 0.0.0.0\n network 1.1.1.0/30 area 0.0.0.0\n network 2.2.2.0/30 area 0.0.0.0" > /etc/frr/frr.conf
+echo "OSPF set for ISP";
+fi;
+if [ "$H" == "HQ-R" ]; then
+echo -e "router ospf 1\n ospf router-id 20.1.1.1\n redistribute connected\n network 20.1.1.0/26 arae 0.0.0.0\n network 1.1.1.0/30 area 0.0.0.0\n network 3.3.3.0/30 area 0.0.0.0" > /etc/frr/frr.conf
+echo "OSPF set for HQ-R";
+fi;
+if [ "$H" == "BR-R" ]; then
+echo -e "router ospf 1\n ospf router-id 30.1.1.1\n redistribute connected\n network 30.1.1.0/28 area 0.0.0.0\n network 3.3.3.0/30 area 0.0.0.0\n network 2.2.2.0/30 area 0.0.0.0" > /etc/frr/frr.conf
+echo "OSPF set for BR-R";
+fi;
+systemctl restart frr
