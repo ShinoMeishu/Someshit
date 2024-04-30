aa13d40412abc6565d524c8e725f456faf652bc0
ShinoMeishu <82775097+ShinoMeishu@users.noreply.github.com>
2024-04-30 16:59:15 +0300

    Update frr_config.sh

--- FRR/frr_config.sh
+++ FRR/frr_config.sh
@@ -7,7 +7,7 @@ chown frr /etc/frr/daemons
 chmod 777 /etc/pam.d/frr
 sed -i 's/ospfd=no/ospfd=yes/' /etc/frr/daemons
 if [ "$H" == "ISP" ]; then
-echo -e "router ospf 1\n ospf router-id 10.1.1.1\n redistribute connected\n network 10.1.1.0/28 area 0.0.0.0\n network 1.1.1.0/30 area 0.0.0.0\n network 2.2.2.0/30 area 0.0.0.0" > /etc/frr/frr.conf
+echo -e "log syslog informational\n\nrouter ospf 1\n ospf router-id 10.1.1.1\n redistribute connected\n network 10.1.1.0/28 area 0.0.0.0\n network 1.1.1.0/30 area 0.0.0.0\n network 2.2.2.0/30 area 0.0.0.0" > /etc/frr/frr.conf
 echo "OSPF set for ISP";
 fi;
 if [ "$H" == "HQ-R" ]; then
@@ -19,3 +19,4 @@ echo -e "router ospf 1\n ospf router-id 30.1.1.1\n redistribute connected\n netw
 echo "OSPF set for BR-R";
 fi;
 systemctl restart frr
+vtysh -c "show run"
