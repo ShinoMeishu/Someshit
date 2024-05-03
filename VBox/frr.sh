fa676a1d9907bc9b17910d5f1e8e216844d934c6
ShinoMeishu <82775097+ShinoMeishu@users.noreply.github.com>
2024-05-03 11:55:04 +0300

    Create frr.sh

---
+++ VBox/frr.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+H=$(hostname)
+apt install frr -y
+#Configuration for Vtysh and ospfd daemon
+chmod 777 /etc/frr/daemons
+chown frr /etc/frr/daemons
+chmod 777 /etc/pam.d/frr
+sed -i 's/ospfd=no/ospfd=yes/' /etc/frr/daemons
+if [ "$H" == "ISP" ]; then
+echo -e "log syslog informational\n\nrouter ospf\n ospf router-id 10.10.10.1\n redistribute connected\n network 10.10.10.0/28 area 0.0.0.0\n network 1.1.1.0/30 area 0.0.0.0\n network 2.2.2.0/30 area 0.0.0.0" > /etc/frr/frr.conf
+echo "OSPF set for ISP";
+fi;
+if [ "$H" == "HQ-R" ]; then
+echo -e "router ospf\n ospf router-id 20.20.20.1\n redistribute connected\n network 20.20.20.0/26 area 0.0.0.0\n network 1.1.1.0/30 area 0.0.0.0" > /etc/frr/frr.conf
+echo "OSPF set for HQ-R";
+fi;
+if [ "$H" == "BR-R" ]; then
+echo -e "router ospf\n ospf router-id 30.1.1.1\n redistribute connected\n network 30.30.30.0/28 area 0.0.0.0\n network 2.2.2.0/30 area 0.0.0.0" > /etc/frr/frr.conf
+echo "OSPF set for BR-R";
+fi;
+sudo systemctl restart frr
+vtysh -c "show run"
+exit 0
